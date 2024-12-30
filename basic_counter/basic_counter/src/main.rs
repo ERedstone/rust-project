e1ad852213e7db7d0f412ba37abecaba2d9b5c85
ERedstone <103945885+ERedstone@users.noreply.github.com>
2024-12-31 00:03:02 +0600

    Update main.rs

--- basic_counter/basic_counter/src/main.rs
+++ basic_counter/basic_counter/src/main.rs
@@ -1,24 +1,13 @@
 use std::io;
 fn main() {
     let mut input = String::new();
-    match io::stdin()
-            .read_line(&mut input) {
-        Ok(..) => (),
-        Err(err) => println!("The following error occurred: {:?}", err),
-    }
-
-    let i = input
-        .trim()
-        .parse()
-        .expect("{}");
-
-        
+    match io::stdin().read_line(&mut input) {Ok(_n) => (),Err(_e) => (),}
+    let i = input.trim().parse().expect("{}");
     let mut count = 0;
     loop {
         count += 1;
-        println!("{}", count);
         if count == i {
-            println!("{}", count);
+            println!("done");
             break;
         }
     }
